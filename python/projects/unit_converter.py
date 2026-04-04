5686520102db7842b5776c575f5c781bd251722d
Justin Losev <justinlosev@gmail.com>
2026-04-04 02:33:45 +0100

    Update changed function naming

--- python/projects/unit_converter.py
+++ python/projects/unit_converter.py
@@ -3,27 +3,27 @@
 # Unit converter for Length, weight and temperature
 import time
 
-def length(km):          # Km to Miles conversion
+def km_to_miles(km):          # Kilometres to Miles conversion
     final_miles = km * 0.621371
     return final_miles
 
-def length2(miles):         # Miles to Km conversion
+def miles_to_km(miles):         # Miles to Kilometres conversion
     final_km = miles * 1.60934
     return final_km
 
-def weight(kg):            # Kg to Lbs conversion
+def kg_to_lbs(kg):            # Kilograms to Pounds conversion
     final_lbs = kg * 2.20462
     return final_lbs
 
-def weight2(lbs):           # Lbs to Kg conversion
+def lbs_to_kg(lbs):           # Pounds to Kilograms conversion
     final_kg = lbs * 0.453592
     return final_kg
 
-def temperature(c):          # Celcius to Fahrenheit conversion
+def celcius_to_fahrenheit(c):          # Celcius to Fahrenheit conversion
     final_f = (c * 9/5) + 32
     return final_f
 
-def temperature2(f):         # Fahrenheit to Celcius
+def fahrenheit_to_celcius(f):         # Fahrenheit to Celcius
     final_c = (f - 32) * 5/9
     return final_c 
 
@@ -66,14 +66,16 @@ while True:
                 print("Too many invalid attempts. Goodbye!")
                 exit()
             time.sleep(1)
+            print()
+            print()
             length_call = int(input("1. Convert Km to Miles     2. Convert Miles to Km     3. Go back   "))
         if length_call == 1:
             km = float(input("How many Kilometres would you like to convert?  "))
-            miles = length(km)
+            miles = km_to_miles(km)
             print(f"You are left with {miles:.2f} miles.")
         elif length_call == 2:
             miles = float(input("How many Miles would you like to convert?    "))
-            km = float(length2(miles))
+            km = miles_to_km(miles)
             print(f"You are left with {km:.2f} kilometres.")
         elif length_call == 3:
             continue
@@ -93,14 +95,16 @@ while True:
                 print("Too many invalid attempts. Goodbye!")
                 exit()
             time.sleep(1)
+            print()
+            print()
             weight_call = int(input("1. Convert kg to lbs   2. Convert lbs to kg    3. Go back  "))
         if weight_call == 1:
             kg = float(input("How many Kilograms are you converting?    "))
-            lbs = weight(kg)
+            lbs = kg_to_lbs(kg)
             print(f"You are left with {lbs:0.2f} pounds.")
         elif weight_call == 2:
             lbs = float(input("How many pounds are you converting?  "))
-            kg = weight2(lbs)
+            kg = lbs_to_kg(lbs)
             print(f"You are left with {kg:0.2f} kilograms.")
         elif weight_call == 3:
             continue
@@ -119,14 +123,16 @@ while True:
                 print("Too many invalid attempts. Goodbye!")
                 exit()
             time.sleep(1)
+            print()
+            print()
             temp_call = int(input("1. Convert from c to f   2. Convert from f to c  3. Go back   "))
         if temp_call == 1:
             c = float(input("What is the Celcius? "))
-            f = temperature(c) 
+            f = celcius_to_fahrenheit(c) 
             print(f"The fahrenheit is {f:.2f}!")
         elif temp_call == 2:
             f = float(input("What is the fahrenheit?  "))
-            c = temperature2(f)
+            c = fahrenheit_to_celcius(f)
             print(f"The celcius is {c:.2f}!")
         elif temp_call == 3:
             continue
